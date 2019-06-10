b9cecb0d231646da4bd663446d5f5478271c04b9
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-10 09:58:32 -0400

    Updates

---
+++ precache-manifest.0cac09dc3c0394bd5222ecc932a00acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0510a376074f2804d4fc654248a01348",
+    "url": "/giphy-api/index.html"
+  },
+  {
+    "revision": "82d2c53e8d1131655145",
+    "url": "/giphy-api/static/css/main.11d16969.chunk.css"
+  },
+  {
+    "revision": "97aed74d3eee820ac53d",
+    "url": "/giphy-api/static/js/2.906af715.chunk.js"
+  },
+  {
+    "revision": "82d2c53e8d1131655145",
+    "url": "/giphy-api/static/js/main.050a5d17.chunk.js"
+  },
+  {
+    "revision": "7f26869ae7d17d36a59e",
+    "url": "/giphy-api/static/js/runtime~main.c9e52869.js"
+  }
+]);
