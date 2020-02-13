691caa24c41498c3f0537c99010351add77f82b4
Jeson Rosario <jesonrosario22@gmail.com>
2020-02-13 17:29:41 -0500

    Made regular search the home page and removed the previous

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Notfound from './notfound';
-import Regular from './regular';
-import Trending from './trending';
-import Random from './random';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+// import App from "./App";
+import Notfound from "./notfound";
+import Regular from "./regular";
+import Trending from "./trending";
+import Random from "./random";
 
 const giphysearch = (
-    <Router>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/regular">Search</Link></li>
-                <li><Link to="/trending">Trending</Link></li>
-                <li><Link to="/random">Random</Link></li>
-            </ul>
-            <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/regular" component={Regular} />
-            <Route path="/trending" component={Trending} />
-            <Route path="/random" component={Random} />
-            <Route component={Notfound} />
-            </Switch>
-        </div>
-    </Router>
-)
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Search</Link>
+        </li>
+        <li>
+          <Link to="/trending">Trending</Link>
+        </li>
+        <li>
+          <Link to="/random">Random</Link>
+        </li>
+      </ul>
+      <Switch>
+        {/* <Route exact path="/" component={App} /> */}
+        <Route exact path="/" component={Regular} />
+        <Route path="/trending" component={Trending} />
+        <Route path="/random" component={Random} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-ReactDOM.render(giphysearch, document.getElementById('root'));
+ReactDOM.render(giphysearch, document.getElementById("root"));
