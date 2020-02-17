85f6b258dd3687f3f38e8ff076c6631f419d646c
Jeson Rosario <jesonrosario22@gmail.com>
2020-02-17 10:48:20 -0500

    Option for title prop

--- src/results.js
+++ src/results.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import "./css/index.css";
 
 function Results(props) {
-    return (
-        <div>
-            <img src={props.i.images.downsized_large.url} alt="default"/>
-        </div>
-    )
+  return (
+    <div>
+      {/* <h2>{props.i.title}</h2> */}
+      <img src={props.i.images.downsized_large.url} alt="default" />
+    </div>
+  );
 }
 
 export default Results;
-
